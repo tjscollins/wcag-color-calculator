220b8375d5c3fc2be4ea360061c545285fb980f1
Tyler Collins <tjscollins@gmail.com>
2017-10-19 22:19:08 +0000

    New functional test for color selector form

--- functional_tests.py
+++ functional_tests.py
@@ -18,6 +18,14 @@ class NewVisitorTest(TestCase):
 
         # I see the page title is "WCAG Text Color Calculator"
         self.assertEqual(self.browser.title, 'WCAG Color Calculator')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('WCAG Color Calculator', header_text)
+
+        # I see a form where I can enter a background color
+        color_selector = self.browser.find_element_by_css_selector(
+            '.bg-color-chooser')
+        self.assertEqual(color_selector.get_attribute('placeholder'),
+                         '#ffffff')
 
         self.fail('Finish the tests')
 
