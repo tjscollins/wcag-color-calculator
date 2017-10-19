d49abad57db49fd5506d500b2fb06ff0d64e1b7f
Tyler Collins <tjscollins@gmail.com>
2017-10-19 22:56:54 +0000

    Add rest_framework

--- color_calculator/urls.py
+++ color_calculator/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from accessible_text_color.views import main_view
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', main_view)
+    url(r'^$', main_view),
+    url(r'^api/', include('rest_framework.urls', namespace='rest_framework')),
 ]
