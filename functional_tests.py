0daaa4c24c8277ea6d070c997ff33775ce2e8bf3
Tyler Collins <tjscollins@gmail.com>
2017-10-19 21:57:57 +0000

    Class based functional test

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,29 @@
-import unittest
+from unittest import TestCase, main
 
 from selenium import webdriver
 
 browser = webdriver.Firefox()
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+
+class NewVisitorTest(TestCase):
+    """Test that page loads correctly with correct content"""
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.close()
+
+    def test_can_enter_a_color_into_form(self):
+        # As an unauthenticated user I can load the page
+        self.browser.get('http://localhost:8000')
+
+        # I see the page title is "WCAG Text Color Calculator"
+        self.assertEqual(self.browser.title, 'WCAG Color Calculator')
+
+        self.fail('Finish the tests')
+
+
+if __name__ == '__main__':
+    main()
