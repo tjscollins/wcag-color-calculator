f62c0b25ee7ca1a238a7b52add3bdd19fd406bb4
Tyler Collins <tjscollins@gmail.com>
2017-10-20 05:34:56 +0000

    Display acceptable color combinations to users

--- static/js/accessible_text_color.js
+++ static/js/accessible_text_color.js
@@ -5,6 +5,9 @@ function submitColors(event) {
     event.preventDefault();
     let bgColor = $('#bg-color').val();
 
+    $('#aa-colors').html('');
+    $('#aaa-colors').html('');
+
     $.ajax({
         type: "post",
         url: "/api/bg-color",
@@ -12,10 +15,27 @@ function submitColors(event) {
             color: bgColor
         },
         dataType: "json",
-        success: function (response) {
-            console.log(response);
-        }
+        success: buildColorList
     });
+}
+
+function buildColorList({ background_color: bgColor, aa_colors: A2Colors, aaa_colors: A3Colors }) {
+    console.log(bgColor, A2Colors, A3Colors);
+
+    let target = $('input[name=target-standards]:checked').val();
+    $(`#${target}-colors`).append(`<h2>WCAG 2.0 Level ${target.toUpperCase()} Color Combinations</h2>`);
+
+    if (target === 'aa') {
+        A2Colors.forEach(resultsTemplate(bgColor, target));
+    } else if (target === 'aaa') {
+        A3Colors.forEach(resultsTemplate(bgColor, target));
+    }
+}
+
+function resultsTemplate(bgColor, target) {
+    return color => {
+        $(`#${target}-colors`).append(`<div class="row color-sample"><div class="col-4"><p>Background Color: ${bgColor} </p><p>Text Color: ${color}</p></div><div class="col-8" style="background-color: ${bgColor};"><p style="color: ${color};">The quick brown fox jumps over the lazy dog.</p></div></div>`);
+    };
 }
 
 $('#bg-color-form').on('submit', submitColors);
