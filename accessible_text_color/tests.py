666cef4b45d2c2ad81068afb9acf877303df3eeb
Tyler Collins <tjscollins@gmail.com>
2017-10-20 01:48:43 +0000

    Test for color and calculator classes

--- accessible_text_color/tests.py
+++ accessible_text_color/tests.py
@@ -1,8 +1,11 @@
-from django.test import TestCase
-from django.urls import resolve
+import json
+
+from django.test import TestCase, Client
+from django.urls import resolve, reverse
 from django.http import HttpRequest
 
-from accessible_text_color.views import main_view
+from accessible_text_color.views import main_view, post_bgcolor
+from accessible_text_color.calculator import Color, TextColorCalculator
 # Create your tests here.
 
 
@@ -19,3 +22,91 @@ class MainViewTests(TestCase):
         self.assertTrue(html.startswith('<!DOCTYPE html>'))
         self.assertIn('<title>WCAG Color Calculator</title>', html)
         self.assertTrue(html.endswith('</html>'))
+
+
+class BgColorAPITests(TestCase):
+
+    def test_api_bgcolor(self):
+        """It should return a list of acceptable text colors for a given
+        background color"""
+
+        client = Client()
+        data = {
+            'color': '#ffffff'
+        }
+        response = client.post('/api/bg-color', data=data)
+
+        self.assertIsNotNone(response)
+        self.assertIs(response.status_code, 200)
+
+
+class ColorTests(TestCase):
+
+    def test_init_method(self):
+        self.assertEqual((171, 205, 239), Color(rgb="#abcdef").rgb)
+        self.assertEqual((100, 100, 100), Color(rgb=(100, 100, 100)).rgb)
+        self.assertEqual((169, 204, 239), Color(hsl=(210, 68, 80)).rgb)
+        self.assertEqual((16, 126, 66), Color(hsl=(147, 77, 28)).rgb)
+
+    def test_rgb_to_hsl(self):
+        calculator = Color("#123456")
+        self.assertEqual((210, 65, 20),
+                         calculator.hsl())
+
+        calculator = Color("#D9832E")
+        self.assertEqual((30, 69, 52),
+                         calculator.hsl())
+
+        calculator = Color("#107D41")
+        self.assertEqual((147, 77, 28),
+                         calculator.hsl())
+
+    def test_luminance(self):
+        self.assertEqual(0, Color('#000000').luminance())
+        self.assertEqual(1, Color('#ffffff').luminance())
+        self.assertEqual(0.7152, Color('#00ff00').luminance())
+        self.assertEqual(0.2126, Color('#ff0000').luminance())
+        self.assertEqual(0.0722, Color('#0000ff').luminance())
+
+    def test_complementary_color(self):
+        self.assertEqual(Color(hsl=(180, 100, 50)).rgb,
+                         Color(hsl=(0, 100, 50)).complementary_color().rgb)
+        self.assertEqual(Color(hsl=(20, 100, 50)).rgb,
+                         Color(hsl=(200, 100, 50)).complementary_color().rgb)
+        self.assertEqual(Color(hsl=(190, 100, 50)).rgb,
+                         Color(hsl=(10, 100, 50)).complementary_color().rgb)
+
+    def test_analogous_colors(self):
+        self.assertEqual([Color(hsl=(105, 100, 50)).rgb,
+                          Color(hsl=(135, 100, 50)).rgb],
+                         list(map(lambda x: getattr(x, 'rgb'),
+                                  Color(hsl=(120, 100, 50)).analogous_colors())
+                              ))
+        self.assertEqual([Color(hsl=(350, 100, 50)).rgb,
+                          Color(hsl=(20, 100, 50)).rgb],
+                         list(map(lambda x: getattr(x, 'rgb'),
+                                  Color(hsl=(5, 100, 50)).analogous_colors())
+                              ))
+        self.assertEqual([Color(hsl=(165, 100, 50)).rgb,
+                          Color(hsl=(195, 100, 50)).rgb],
+                         list(map(lambda x: getattr(x, 'rgb'),
+                                  Color(hsl=(180, 100, 50)).analogous_colors())
+                              ))
+
+
+class TextColorCalculatorTests(TestCase):
+
+    def test_contrast_calculation(self):
+        calculator = TextColorCalculator()
+
+        self.assertAlmostEqual(21,
+                               calculator.constrast(Color(rgb="#00000"),
+                                                    Color(hsl=(0, 0, 100))))
+
+        self.assertAlmostEqual(5,
+                               calculator.constrast(Color(rgb="#cc3922"),
+                                                    Color(hsl=(120, 50, 100))))
+
+        self.assertAlmostEqual(1,
+                               calculator.constrast(Color(rgb="#ffffff"),
+                                                    Color(hsl=(0, 0, 100))))
