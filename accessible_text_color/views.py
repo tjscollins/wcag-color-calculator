3118742a119239bf6880728f7de2a99165af0b18
Tyler Collins <tjscollins@gmail.com>
2017-10-20 04:58:53 +0000

    Convert colors to strings before sending response

--- accessible_text_color/views.py
+++ accessible_text_color/views.py
@@ -4,8 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-from accessible_text_color.calculator import TextColorCalculator
-# Create your views here.
+from accessible_text_color.calculator import Color, TextColorCalculator
 
 
 def main_view(request):
@@ -22,6 +21,13 @@ def main_view(request):
 @api_view(['POST'])
 def post_bgcolor(request):
     """Take a bg color, return a list of acceptable text colors"""
-    bg_color = request.data.get('color')
+    bg_color = Color(request.data.get('color'))
 
-    return Response({'color': bg_color})
+    aa_colors = list(map(str, TextColorCalculator().AA_textcolors(bg_color)))
+    aaa_colors = list(map(str, TextColorCalculator().AAA_textcolors(bg_color)))
+
+    return Response({
+        'background_color': str(bg_color),
+        'aa_colors': aa_colors,
+        'aaa_colors': aaa_colors,
+    })
