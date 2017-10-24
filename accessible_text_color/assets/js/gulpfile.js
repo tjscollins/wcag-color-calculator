bbc75e87963afbbb0255be5ed656547f18fe3485
Tyler Collins <tjscollins@gmail.com>
2017-10-24 04:20:40 +0000

    Improve JS bundling

--- accessible_text_color/assets/js/gulpfile.js
+++ accessible_text_color/assets/js/gulpfile.js
@@ -3,18 +3,30 @@ const livereload = require('gulp-livereload');
 const rollup = require('gulp-better-rollup');
 const babel = require('rollup-plugin-babel');
 const rename = require('gulp-rename');
+const nodeResolve = require('rollup-plugin-node-resolve');
+const commonjs = require('rollup-plugin-commonjs');
 
 gulp.task('rollup', () => {
     gulp.src('src/index.js')
-    .pipe(rollup({
-        plugins: [babel()]
-    }, {
-        format: 'iife'
-    }))
-    .pipe(rename({
-        basename: 'accessible_text_color'
-    }))
-    .pipe(gulp.dest('../../../static/js/'))
+        .pipe(rollup({
+            plugins: [babel({
+                babelrc: true,
+                comments: true,
+                runtimeHelpers: true
+            }),
+            nodeResolve({
+                module:true,
+                browser:true,
+            }),
+            commonjs(),
+        ]
+        }, {
+            format: 'iife'
+        }))
+        .pipe(rename({
+            basename: 'accessible_text_color'
+        }))
+        .pipe(gulp.dest('../../../static/js/'))
 });
 
 gulp.task('watch', ['rollup'], () => {
