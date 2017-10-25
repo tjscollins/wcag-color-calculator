f2913dd046fcdefd4309d9a04aeeb2b1df8c32c7
Tyler Collins <tjscollins@gmail.com>
2017-10-25 22:16:59 +0000

    closure compiler build support

--- accessible_text_color/assets/js/gulpfile.js
+++ accessible_text_color/assets/js/gulpfile.js
@@ -5,6 +5,7 @@ const babel = require('rollup-plugin-babel');
 const rename = require('gulp-rename');
 const nodeResolve = require('rollup-plugin-node-resolve');
 const commonjs = require('rollup-plugin-commonjs');
+const closure = require('gulp-closure-compiler'); 
 
 gulp.task('rollup', () => {
     gulp.src('src/index.js')
@@ -27,6 +28,38 @@ gulp.task('rollup', () => {
             basename: 'accessible_text_color'
         }))
         .pipe(gulp.dest('../../../static/js/'))
+});
+
+gulp.task('production', () => {
+    gulp.src('src/index.js')
+    .pipe(rollup({
+        plugins: [babel({
+            babelrc: true,
+            comments: true,
+            runtimeHelpers: true
+        }),
+        nodeResolve({
+            module:true,
+            browser:true,
+        }),
+        commonjs(),
+    ]
+    }, {
+        format: 'iife'
+    }))
+    .pipe(closure({
+        compilerPath: 'node_modules/google-closure-compiler/compiler.jar',
+        filename: 'build.js',
+        compilerFlags: {
+            closure_entry_point: 'app.main',
+            compilation_level: 'ADVANCED',
+            warning_level: 'VERBOSE'
+          }
+    }))
+    .pipe(rename({
+        basename: 'accessible_text_color'
+    }))
+    .pipe(gulp.dest('../../../static/js/'))
 });
 
 gulp.task('watch', ['rollup'], () => {
